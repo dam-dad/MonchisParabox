a854e82de4bcbe2bfb0da01f87b7611c08579aa0
Yorman <yormansantos10@hotmail.com>
2024-01-24 09:36:46 +0000

    Initial code
    Subida de código inicial

--- src/main/java/dad/monchisparabox/App.java
+++ src/main/java/dad/monchisparabox/App.java
@@ -1,13 +1,20 @@
 package dad.monchisparabox;
 
+import dad.monchisparabox.uicontroller.MainController;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
+    private static MainController mainController;
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
-	}
+		mainController = new MainController();
 
+        primaryStage.setTitle("Monchi's Parabox");
+        primaryStage.setScene(new Scene(mainController.getView()));
+        primaryStage.show();
+	}
 }
---
+++ src/main/java/dad/monchisparabox/uicontroller/MainController.java
@@ -0,0 +1,38 @@
+package dad.monchisparabox.uicontroller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+
+public class MainController implements Initializable {
+	
+    @FXML
+    private BorderPane view;
+    
+	public MainController() {
+		try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
+            loader.setController(this);
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		/*
+		 * Aquí se establece lo que queremos mostrar...
+		 * getView().setCenter(new MapController().getView());
+		 */
+	}
+
+	public BorderPane getView() {
+		return view;
+	}
+}
